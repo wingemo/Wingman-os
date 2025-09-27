a02da2e1dce394a3fa47e7529e381760ffb5ae70
Wingemo <philip.wingemo@outlook.com>
2025-09-27 11:47:25 +0200

    Update boot.asm

--- boot.asm
+++ boot.asm
@@ -1,22 +1,35 @@
 ; boot.asm
 ; En 16-bitars bootloader som BIOS laddar på 0x7C00.
-; Den skriver "hello world" via BIOS teletype (int 0x10).
+; Den skriver "hello world" via BIOS teletype (int 0x10)
+; och stänger sedan av maskinen via APM.
 
 BITS 16
 ORG 0x7C00
 
 start:
-    mov si, msg       ; pekar på strängen
-    mov ah, 0x0E      ; BIOS teletype
+    ; skriv strängen
+    mov si, msg
+    mov ah, 0x0E
 
 .print:
-    lodsb             ; läs byte från [SI] -> AL
+    lodsb
     cmp al, 0
     je .done
-    int 0x10          ; skriv tecken
+    int 0x10
     jmp .print
 
 .done:
+    ; försök stänga av via APM
+    mov ax, 0x5301     ; APM Installation Check
+    xor bx, bx
+    int 0x15
+
+    mov ax, 0x5307     ; APM Set Power State
+    mov bx, 1          ; all devices
+    mov cx, 3          ; power off
+    int 0x15
+
+    ; om avstängning misslyckas -> häng CPU
     cli
     hlt
 
